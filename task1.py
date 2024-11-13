43fb1a7e0263857fe2e2d8803a90a54a37c13141
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2024-11-12 19:18:07 -0800

    done t1

--- task1.py
+++ task1.py
@@ -38,9 +38,14 @@ table = [
 ]
 
 def target(lvl,ac):
-    return
+    op_ac = 10 - ac
+    #print (op_ac)
+    return table[lvl][op_ac]
 
-def tests():
+def main():
     assert target(5,7) == 11
     assert target(10,-10) == 20
     assert target(17,-3) == 13
+    
+if __name__ == "__main__":
+    main()
